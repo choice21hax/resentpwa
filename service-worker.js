033dc6e3fdd887f7ee8edc002c4c1e52b2d3cab2
choice21 <storageusmanau@gmail.com>
2024-12-20 08:30:39 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "eaglercraft-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/main.js",
+    "/assets/",
+    "/lang/"
+    "/assets.epk"
+    "/classes.js"
+    "/favicon.png"
+    "/manifest.json"
+    "/README.md"
+    "/service-worker.js"
+];
+
+// Install the Service Worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate the Service Worker
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
